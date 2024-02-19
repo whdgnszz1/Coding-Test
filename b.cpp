9b7bfc3f9e02d1d9d30edb091143233115adb756
whdgnszz1 <whdgnszz1@snu.ac.kr>
2024-02-20 00:06:52 +0900

    Permutation

---
+++ b.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+// int main () {
+//   int a[] = {1, 2, 3};
+//   do {
+//     for(int i : a) cout << i << " ";
+//     cout << '\n';
+//   } while(next_permutation(&a[0], &a[0] + 3));
+// }
+int main () {
+  vector<int> a = {2, 1, 3, 100, 200};
+  sort(a.begin(), a.end());
+  do {
+    for(int i = 0; i < 2; i++) {
+      cout << a[i] << " ";
+    }
+    cout << '\n';
+  } while(next_permutation(a.begin(), a.end()));
+}
