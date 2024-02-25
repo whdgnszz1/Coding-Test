e5c95fb5cfad9e801b80203e89950c7b59877966
whdgnszz1 <whdgnszz1@snu.ac.kr>
2024-02-25 20:11:53 +0900

    Array to decay

--- g.cpp
+++ g.cpp
@@ -1,10 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+// Array to decay는 vector타입은 안되고, 정적 배열만 가능하다.
+int a[3] = {1, 2, 3};
 int main() {
-  string a = "kundol";
-  string *b = &a;
-  cout << b << '\n';
-  cout << *b << '\n';
+  // string a = "kundol";
+  // string *b = &a;
+  // cout << b << '\n';
+  // cout << *b << '\n';
+  // return 0;
+  int * c = a;
+  cout << c << "\n";
+  cout << &a[0] << "\n";
+  cout << c + 1 << "\n";
+  cout << &a[1] << "\n";
   return 0;
 }
