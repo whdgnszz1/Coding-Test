d412efed9a26880824606aaabf51c50d8d2a7658
whdgnszz1 <whdgnszz1@snu.ac.kr>
2024-03-09 21:29:14 +0900

    Array[:: -1]

--- python/11.py
+++ python/11.py
@@ -6,11 +6,15 @@ n = int(input())
 for i in range(n):
   s = input()
   s = s.upper()
-  size = len(s)
-  for j in range(size // 2):
-    if s[j] != s[-1 -j]:
-      print("#%d NO" %(i + 1))
-      break
-    else:
-      print("#%d YES" %(i + 1))
-      break
+  # size = len(s)
+  # for j in range(size // 2):
+  #   if s[j] != s[-1 -j]:
+  #     print("#%d NO" %(i + 1))
+  #     break
+  #   else:
+  #     print("#%d YES" %(i + 1))
+  #     break
+  if s == s[:: -1]:
+    print("#%d YES" %(i + 1))
+  else:
+    print("#%d NO" %(i + 1))
