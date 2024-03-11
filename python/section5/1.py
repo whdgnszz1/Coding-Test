b4341c34baa02c422b2df073ad55d97833155d81
whdgnszz1 <whdgnszz1@snu.ac.kr>
2024-03-11 22:11:28 +0900

    Print binary number using recursion

---
+++ python/section5/1.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+def DFS(x):
+  if x == 0:
+    return
+  else:
+    DFS(x // 2)
+    print(x % 2, end = "")
+
+if __name__=="__main__":
+  n = int(input())
+  DFS(n)
